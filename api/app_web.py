0e8f480f0b503c6405bb505479ef2bf442f010bb
NDOudi <princygrace242@gmail.com>
2025-06-23 10:57:41 +0200

    Mise à jour du site

--- api/app_web.py
+++ api/app_web.py
@@ -7,8 +7,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 
-from sentence_transformers import SentenceTransformer, util
-import torch
 
  
 st.set_page_config(page_title="NetView : Analyse des Logs Windows", layout="wide")
